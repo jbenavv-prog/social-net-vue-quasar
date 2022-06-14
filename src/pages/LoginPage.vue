7b2cafb1eccea8ff6cf2807ef237024856863d25
Julian Camilo Benavides Vanegas <julian.benavides@comware.com.co>
2022-06-14 13:21:51 -0500

    route login

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="h-100">
+    <div>hello Login page works</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoginPage",
+});
+</script>
+
+<style>
+.h-100 {
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.7),
+      rgba(255, 255, 255, 0.4)
+    ),
+    url("~assets/images/login/bg-01.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+}
+</style>
