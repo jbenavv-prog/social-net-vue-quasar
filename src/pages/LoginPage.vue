4a6bfea03a017e1c59f90c8981bb76d3d54eada6
Julian Camilo Benavides Vanegas <julian.benavides@comware.com.co>
2022-06-14 16:14:02 -0500

    Login Desing Form Completed

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,14 +1,47 @@
 <template>
   <div class="h-100">
     <div class="q-pa-md row items-start q-gutter-md">
-      <q-card class="my-card">
+      <q-card class="my-card q-pa-lg">
         <q-card-section>
           <div class="text-h6 text-center">Iniciar Sesión</div>
           <!-- <div class="text-subtitle2">by John Doe</div> -->
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          {{ lorem }}
+          <div class="q-pa-md" style="max-width: 400px">
+            <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+              <q-input
+                v-model="email"
+                filled
+                type="email"
+                hint="Email"
+                :rules="[(val) => !!val || 'Email es requerido']"
+              />
+              <q-input
+                v-model="password"
+                filled
+                :type="isPwd ? 'password' : 'text'"
+                hint="Contraseña"
+                :rules="[(val) => !!val || 'Contraseña es requerida']"
+              >
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                  />
+                </template>
+              </q-input>
+              <div>
+                <q-btn
+                  class="full-width q-mt-xl"
+                  label="Iniciar sesión"
+                  type="submit"
+                  color="primary"
+                />
+              </div>
+            </q-form>
+          </div>
         </q-card-section>
       </q-card>
     </div>
@@ -17,13 +50,39 @@
 
 <script>
 import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
+import { ref } from "vue";
 
 export default defineComponent({
   name: "LoginPage",
   setup() {
+    const $q = useQuasar();
+
+    const email = ref(null);
+    const password = ref(null);
+
     return {
-      lorem:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      email,
+      password,
+      isPwd: ref(true),
+
+      onSubmit() {
+        if (1 != 1) {
+          $q.notify({
+            color: "red-5",
+            textColor: "white",
+            icon: "warning",
+            message: "You need to accept the license and terms first",
+          });
+        } else {
+          $q.notify({
+            color: "green-4",
+            textColor: "white",
+            icon: "cloud_done",
+            message: "Submitted",
+          });
+        }
+      },
     };
   },
 });
