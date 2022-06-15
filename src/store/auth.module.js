3923dcd2c1884c3985e4697b91811a9c26479ee2
jbenavv-prog <correoj94@gmail.com>
2022-06-15 13:32:14 -0500

    Login Completed

--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -1,24 +1,45 @@
-import ApiService from "../../common/apiService";
+import ApiService from "../../common/api.service";
+import JwtService from "../../common/jwt.service";
 import { LOGIN } from "./actions.type";
+import { SET_AUTH } from "./mutations.type";
+
+const state = {
+  errors: null,
+  user: {},
+  isAuthenticated: !!JwtService.getToken(),
+};
 
 const actions = {
   [LOGIN](context, credentials) {
     return new Promise((resolve) => {
       ApiService.post("auth/authenticate", credentials)
-        .then(({ data }) => {
-          if (data.ok) {
-            resolve(data);
+        .then(({ data: response }) => {
+          if (response.ok) {
+            context.commit(SET_AUTH, response.data);
+            resolve(response);
           } else {
-            console.log("into false error");
+            console.log(response.message);
           }
         })
         .catch((response) => {
+          console.log(response);
           console.log("Fallo del sistema");
         });
     });
   },
 };
 
+const mutations = {
+  [SET_AUTH](state, user) {
+    state.isAuthenticated = true;
+    state.user = user;
+    state.errors = {};
+    JwtService.saveToken(state.user.token);
+  },
+};
+
 export default {
+  state,
   actions,
+  mutations,
 };
